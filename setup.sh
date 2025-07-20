2f19bf37c676542267cd546c1a710fca1f6d2cf9
Marco Busch <marco.busch@tum.de>
2025-07-20 15:58:44 +0200

    add setup for cluster

---
+++ setup.sh
@@ -0,0 +1,4 @@
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu124
+pip install -r requirements.txt
+export HF_HOME="/mnt/projects/mlmi/dmcaf/models/hf_home"
+export TORCH_HOME="/mnt/projects/mlmi/dmcaf/models/torch_home"
